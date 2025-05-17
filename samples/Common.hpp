f9bab280fe9690fbb312ec3a09ecacdfc77c8f96
Adrien ARNAUD <arnaudrien1@free.fr>
2025-05-17 13:45:10 +0100

    Change license

--- samples/Common.hpp
+++ samples/Common.hpp
@@ -1,102 +1,28 @@
 /*
- * Copyright (C) 2025 Adrien ARNAUD
+ * Copyright (c) 2025 Adrien ARNAUD
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #pragma once
 
 #include <vkw/vkw.hpp>
-
-namespace vkw
-{
-struct BufferPropertyFlags
-{
-    VkBufferUsageFlags usage;
-    VkMemoryPropertyFlags memoryFlags;
-};
-struct ImagePropertyFlags
-{
-    VkImageUsageFlags usage;
-    VkMemoryPropertyFlags memoryFlags;
-};
-} // namespace vkw
-
-// -----------------------------------------------------------------------------
-
-static constexpr vkw::BufferPropertyFlags hostStagingFlags
-    = {VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
-       VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT};
-
-static constexpr vkw::BufferPropertyFlags deviceFlags
-    = {VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT
-           | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
-       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT};
-
-static constexpr vkw::BufferPropertyFlags uniformDeviceFlags
-    = {VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT};
-
-static constexpr vkw::BufferPropertyFlags uniformHostStagingFlags
-    = {VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT
-           | VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-       VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT};
-
-static constexpr vkw::ImagePropertyFlags imgDeviceFlags = {
-    VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
-    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT};
-
-static constexpr vkw::BufferPropertyFlags vertexBufferFlags
-    = {VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT};
-
-static constexpr vkw::BufferPropertyFlags indexBufferFlags
-    = {VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
-       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT};
-
-// -----------------------------------------------------------------------------
-
-template <typename T>
-static inline T randVal()
-{
-    return 2.0f * float(rand()) / float(RAND_MAX) - 1.0f;
-}
-
-template <typename T>
-static std::vector<T> randArray(const size_t size)
-{
-    std::vector<T> ret(size);
-    for(size_t i = 0; i < size; i++)
-    {
-        ret[i] = randVal<T>();
-    }
-    return ret;
-}
-
-template <typename T>
-static bool compareArrays(std::vector<T>& v0, std::vector<T>& v1)
-{
-    assert(v0.size() == v1.size());
-    for(size_t i = 0; i < v0.size(); i++)
-    {
-        if(v0[i] != v1[i])
-        {
-            return false;
-        }
-    }
-    return true;
-}
 
 template <typename T>
 void uploadData(vkw::Device& device, const T* srcPtr, vkw::DeviceBuffer<T>& dst)
